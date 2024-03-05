d8dc0ba2dc94bba241e1b3607d31f867301de275
vineethayasa <vineethareddy185@gmail.com>
2024-03-05 18:34:51 +0530

    new c5

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-const { dirname } = require('path');
-const app = require('./app');
+const app = require("./app");
 const port = process.env.PORT || 3000;
-
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
-
-const options = {
-  key: fs.readFileSync(path.join(__dirname,'cert','key.pem')),
-  cert: fs.readFileSync(path.join(__dirname,'cert','cert.pem')),
-};
-
-const server = https.createServer(options, app);
-
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`server listening at port - ${port}`);
 });
