4db485b3b11e60fc5296ac771f6d0a7da22022a8
vineethayasa <vineethareddy185@gmail.com>
2024-04-20 20:42:45 +0530

    new

--- i18n.js
+++ i18n.js
@@ -1,16 +1,16 @@
 // i18n.js
-const i18next = require('i18next');
-const Backend = require('i18next-fs-backend');
-const middleware = require('i18next-http-middleware');
+const i18next = require("i18next");
+const Backend = require("i18next-fs-backend");
+const middleware = require("i18next-http-middleware");
 
 i18next
-  .use(Backend)
-  .use(middleware.LanguageDetector)
-  .init({
-    fallbackLng: 'en',
-    backend: {
-      loadPath: './locale/{{lng}}.json',
-    }
-  });
+	.use(Backend)
+	.use(middleware.LanguageDetector)
+	.init({
+		fallbackLng: "en",
+		backend: {
+			loadPath: "./locale/{{lng}}.json",
+		}
+	});
 
 module.exports = i18next;
